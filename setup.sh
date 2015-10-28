f37246d94ddb14a14633747de7c7690da09fae33
Sergio Spinatelli <s.spinatelli@reply.de>
2015-10-28 11:18:03 +0100

    Creating tmp folder in setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ sudo npm install grunt-cli express bower -g
 echo 'export PATH=$PATH:/opt/crossbar/bin' > /home/vagrant/.bashrc
 source /home/vagrant/.bashrc
 touch /home/vagrant/run_me.sh
-echo "mkdir /home/vagrant/tmp" >> /home/vagrant/run_me.sh
+mkdir /home/vagrant/tmp
 echo "git clone https://github.com/DataReply/Columba.git tmp/Columba" >> /home/vagrant/run_me.sh
 echo "cd tmp/Columba" >> /home/vagrant/run_me.sh
 echo "git checkout devbox" >> /home/vagrant/run_me.sh
@@ -23,8 +23,9 @@ echo "cp tmp/Columba/* Columba/ -R" >> /home/vagrant/run_me.sh
 echo "cp tmp/Columba/.gitignore Columba" >> /home/vagrant/run_me.sh
 echo "cp tmp/Columba/.gitattributes Columba" >> /home/vagrant/run_me.sh
 echo "cp tmp/Columba/.git Columba/ -R" >> /home/vagrant/run_me.sh
-echo "cd Columba/frontend" >> /home/vagrant/run_me.sh
+echo "cd Columba" >> /home/vagrant/run_me.sh
+echo "mv frontend/package.json ." >> /home/vagrant/run_me.sh
 echo "npm install" >> /home/vagrant/run_me.sh
 sudo chmod +x /home/vagrant/run_me.sh
-sudo chown vagrant:vagrant /home/vagrant/tmp
+sudo chown vagrant:vagrant /home/vagrant/tmp -R
 sudo chown vagrant:vagrant /home/vagrant/run_me.sh
