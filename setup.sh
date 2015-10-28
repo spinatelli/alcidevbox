87b4dfe3ac88ab1d6f3ee84c6ffc7c5e9e7c4269
Sergio Spinatelli <s.spinatelli@reply.de>
2015-10-28 15:42:01 +0100

    Added elasticsearch to setup script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,24 @@
 sudo apt-key adv --keyserver hkps.pool.sks-keyservers.net --recv D58C6920
+sudo add-apt-repository ppa:webupd8team/java -y
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 sudo sh -c "echo 'deb http://package.crossbar.io/ubuntu trusty main' > /etc/apt/sources.list.d/crossbar.list"
 curl -sL https://deb.nodesource.com/setup_iojs_3.x | sudo -E bash -
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
-sudo apt-get install -y crossbar python-pip python3-numpy python3 python3-pip git iojs
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
+sudo apt-get install -y crossbar python-pip python3-numpy python3 python3-pip git iojs oracle-java8-installer
 sudo pip3 install pandas autobahn ujson asyncio
 
 sudo npm install -g npm@latest
 sudo npm install grunt-cli express bower -g
 echo 'export PATH=$PATH:/opt/crossbar/bin' > /home/vagrant/.bashrc
 source /home/vagrant/.bashrc
+
+curl -L -O https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.3.deb
+sudo dpkg -i elasticsearch-1.7.3.deb
+sudo update-rc.d elasticsearch defaults 95 10
+sudo /etc/init.d/elasticsearch start
+curl -XPUT 'localhost:9200/device?pretty'
+
 touch /home/vagrant/run_me.sh
 mkdir /home/vagrant/tmp
 echo "git clone https://github.com/DataReply/Columba.git tmp/Columba" >> /home/vagrant/run_me.sh
