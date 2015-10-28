3b683ba19b0a0b101182cede34a6461fc87b6206
Sergio Spinatelli <s.spinatelli@reply.de>
2015-10-28 10:05:31 +0100

    Changed setup script

--- setup.sh
+++ setup.sh
@@ -9,20 +9,22 @@ sudo pip3 install anaconda pandas autobahn ujson asyncio
 
 sudo npm install -g npm@latest
 sudo npm install grunt-cli express bower -g
-echo 'export PATH=$PATH:/opt/crossbar/bin' > ~/.bashrc
-source ~/.bashrc
-mkdir ~/tmp
-touch ~/run_me.sh
-echo "git clone <repo URL> tmp/Columba
-cd tmp/Columba
-git checkout devbox
-cd ~
-sudo chown vagrant ./Columba -R
-sudo chmod 775 ./Columba -R
-cp tmp/Columba/* Columba/ -R
-cp tmp/Columba/.gitignore Columba
-cp tmp/Columba/.gitattributes Columba
-cp tmp/Columba/.git Columba/ -R
-cd Columba/frontend
-npm install" >> ~/run_me.sh
-sudo chmod +x ~/run_me.sh
+echo 'export PATH=$PATH:/opt/crossbar/bin' > /home/vagrant/.bashrc
+source /home/vagrant/.bashrc
+touch /home/vagrant/run_me.sh
+echo "mkdir /home/vagrant/tmp" >> /home/vagrant/run_me.sh
+echo "git clone https://github.com/DataReply/Columba.git tmp/Columba" >> /home/vagrant/run_me.sh
+echo "cd tmp/Columba" >> /home/vagrant/run_me.sh
+echo "git checkout devbox" >> /home/vagrant/run_me.sh
+echo "cd /home/vagrant" >> /home/vagrant/run_me.sh
+echo "sudo chown vagrant ./Columba -R" >> /home/vagrant/run_me.sh
+echo "sudo chmod 775 ./Columba -R" >> /home/vagrant/run_me.sh
+echo "cp tmp/Columba/* Columba/ -R" >> /home/vagrant/run_me.sh
+echo "cp tmp/Columba/.gitignore Columba" >> /home/vagrant/run_me.sh
+echo "cp tmp/Columba/.gitattributes Columba" >> /home/vagrant/run_me.sh
+echo "cp tmp/Columba/.git Columba/ -R" >> /home/vagrant/run_me.sh
+echo "cd Columba/frontend" >> /home/vagrant/run_me.sh
+echo "npm install" >> /home/vagrant/run_me.sh
+sudo chmod +x /home/vagrant/run_me.sh
+sudo chown vagrant:vagrant /home/vagrant/tmp
+sudo chown vagrant:vagrant /home/vagrant/run_me.sh
